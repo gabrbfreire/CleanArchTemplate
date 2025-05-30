701a08f7e436dcadb4f97a8975fda5a20362b87d
Gabriel Freire <gabrbfreire@gmail.com>
2025-05-30 16:53:20 -0300

    Add project files.

---
+++ SimplifiedCleanArchModel.Api/Program.cs
@@ -0,0 +1,38 @@
+
+namespace SimplifiedCleanArchModel
+{
+    public class Program
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.MapGet("/weatherForecast/getWeatherForecast", () =>
+            {
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.Run();
+        }
+    }
+}
